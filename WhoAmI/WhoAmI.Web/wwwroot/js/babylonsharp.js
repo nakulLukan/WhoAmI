49356f7637b1f3befed5a9ec5d29b0e2b76cdfbc
Nakul A <matrixlukan@gmail.com>
2022-01-19 22:06:45 +0530

    Suzane added as logo

--- WhoAmI/WhoAmI.Web/wwwroot/js/babylonsharp.js
+++ WhoAmI/WhoAmI.Web/wwwroot/js/babylonsharp.js
@@ -1,18 +1,16 @@
 ﻿import '../lib/babylonjs/babylon.js'
 
-export function initialiseScene() {
-    const canvas = document.getElementById("footer-canvas");
+export function initialiseScene(canvasElementName, assetName) {
+    const canvas = document.getElementById(canvasElementName);
     const engine = new BABYLON.Engine(canvas, true);
     const createScene = function () {
         const scene = new BABYLON.Scene(engine);
+        scene.clearColor = new BABYLON.Color3.FromHexString('#0082e6');
+        BABYLON.SceneLoader.ImportMeshAsync("", "../assets/", assetName + '.babylon');
 
-        BABYLON.SceneLoader.ImportMeshAsync("", "../assets/", "cude.babylon");
-
-        const camera = new BABYLON.ArcRotateCamera("camera", -Math.PI / 2, Math.PI / 2.5, 15, new BABYLON.Vector3(0, 0, 0));
+        const camera = new BABYLON.ArcRotateCamera("camera", -Math.PI / 2, Math.PI / 2.5, 4, new BABYLON.Vector3(0, 0, 0));
         camera.attachControl(canvas, true);
         const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(1, 1, 0));
-
-        console.log(scene.meshes);
 
         return scene;
     };
