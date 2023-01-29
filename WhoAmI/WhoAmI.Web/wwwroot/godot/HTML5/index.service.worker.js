11b80910e8036c08783531234e856e12b238d525
Nakul A <matrixlukan@gmail.com>
2023-01-29 12:00:21 +0530

    [Godot] Clouds added. - Camera zoom-in, zoom-out implemented

--- WhoAmI/WhoAmI.Web/wwwroot/godot/HTML5/index.service.worker.js
+++ WhoAmI/WhoAmI.Web/wwwroot/godot/HTML5/index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1674894167|7237874564";
+const CACHE_VERSION = "1674973362|3282967908";
 const CACHE_PREFIX = "Who Am I. Intera-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
--- WhoAmI/godot/WhoAmI.Interactive/Scripts/Player/PlayerNavigation.cs
+++ WhoAmI/godot/WhoAmI.Interactive/Scripts/Player/PlayerNavigation.cs
@@ -7,19 +7,24 @@ public class PlayerNavigation : KinematicBody
     [Export] public float GravitationalForce { get; set; } = 9.8F;
     [Export] public float MovementSpeed { get; set; } = 0.2F;
     [Export] public float RotationSpeed { get; set; } = 10F;
+    [Export] public int MaxZoomOut { get; set; } = 35;
+    [Export] public int MinZoomOut { get; set; } = 15;
+    [Export] public float ZoomSpeed { get; set; } = 0.4f;
 
     Spatial gravity = null;
     Spatial subPlayer = null;
+    Spatial cameraPositionTarget;
     float time = 0;
-    
+
     Vector2 keyboardAction = Vector2.Zero;
 
     bool isCameraInActive = false;
-    
+
     public override void _Ready()
     {
         gravity = GetTree().Root.GetNode<Spatial>(NodePath.GravityPoint);
         subPlayer = GetNode<Spatial>(NodePath.SubPlayer);
+        cameraPositionTarget = GetNode<Spatial>(NodePath.PlayerCameraPosition);
     }
 
     public override void _PhysicsProcess(float delta)
@@ -68,6 +73,30 @@ public class PlayerNavigation : KinematicBody
                     keyboardAction.x = isPressed ? 1 : 0;
                     break;
             }
+        }
+
+        if (@event is InputEventMouseButton mouseEvent)
+        {
+            switch ((ButtonList)mouseEvent.ButtonIndex)
+            {
+                case ButtonList.WheelDown:
+                    ZoomCamera(1);
+                    break;
+                case ButtonList.WheelUp:
+                    ZoomCamera(-1);
+                    break;
+            }
+        }
+    }
+
+    private void ZoomCamera(int zoomDirection)
+    {
+        // Move camera target position forward or backward.
+        cameraPositionTarget.GlobalTranslate(cameraPositionTarget.GlobalTransform.basis.z * ZoomSpeed * zoomDirection);
+        if (cameraPositionTarget.Translation.z < MinZoomOut || cameraPositionTarget.Translation.z > MaxZoomOut)
+        {
+            var z = cameraPositionTarget.Translation.z - Math.Max(MinZoomOut, Math.Min(cameraPositionTarget.Translation.z, MaxZoomOut));
+            cameraPositionTarget.Translation += Vector3.Forward * z;
         }
     }
 
